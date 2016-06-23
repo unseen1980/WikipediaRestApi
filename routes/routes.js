19b95dcdb36c6406597d375c1ebec21b7c6b97cf
Christos Koutsiaris <christos.koutsiaris@aerlingus.com>
2016-06-23 15:30:19 +0100

    observables in place

--- routes/routes.js
+++ routes/routes.js
@@ -1,22 +1,39 @@
-var request = require('request');
-var appRouter = function(app) {
-  app.get("/", function(req, res) {
-    if (!req.query.wiki) {
-      return res.send({
-        "status": "error",
-        "message": "missing request info"
-      });
-    } else {
-      request('https://en.wikipedia.org/w/api.php?action=query&prop=extracts&format=json&exintro=1&explaintext=&titles=' + req.query.wiki, function(error, response, body) {
-        if (!error && response.statusCode == 200) {
-          var c = JSON.parse(body),
-            grabPage = Object.keys(c.query.pages)[0];
-
-          return res.send(c.query.pages[grabPage].extract);
-        }
-      });
-    }
-  });
-}
+var request = require('request'),
+  Rx = require('rx'),
+  appRouter = function (app) {
+    app.get("/", function (req, res) {
+      if (!req.query.wiki) {
+        return res.send({
+          "status": "error",
+          "message": "missing request info"
+        });
+      } else {
+        var wikiReq = Rx.Observable.fromNodeCallback(request.get),
+          call = wikiReq([
+            'https://en.wikipedia.org/w/api.php',
+            '?action=query',
+            '&prop=extracts',
+            '&format=json',
+            '&exintro=1',
+            '&explaintext=',
+            '&titles=',
+            req.query.wiki].join('')),
+          subscription = call
+            .subscribe(
+            (wikiRes) => {
+              return res.send(
+                JSON.parse(wikiRes[1]).query.pages[
+                  Object.keys(JSON.parse(wikiRes[1]).query.pages)[0]
+                ].extract);
+            },
+            (err) => {
+              console.log('Error: ' + err);
+            },
+            () => {
+              console.log('Completed');
+            });
+      }
+    });
+  }
 
 module.exports = appRouter;
